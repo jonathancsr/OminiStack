1899062354ebc24848139bb5ac938502000044ae
jonathancsr <jonathancsrr@gmail.com>
2019-08-09 21:21:41 -0300

    Inicio de backend

---
+++ backend/src/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const mongoose = require('mongoose');
+
+const routes = require('./routes');
+
+const server = express();
+
+mongoose.connect('mongodb+srv://oministack:oministack@cluster0-fb2v2.mongodb.net/oministack8?retryWrites=true&w=majority', { useNewUrlParser: true });
+
+server.use(express.json());
+server.use(routes);
+
+server.listen(3333);
+
