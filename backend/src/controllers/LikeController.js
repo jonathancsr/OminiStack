3c03078200a591ebbaecc54da2c73cda0237607a
jonathancsr <jonathancsrr@gmail.com>
2019-08-11 08:48:52 -0300

    BackEnd completo

---
+++ backend/src/controllers/LikeController.js
@@ -0,0 +1,27 @@
+const Dev = require('../models/Dev')
+
+module.exports = {
+    async store(req, res) {
+        console.log(req.params.devId);
+
+        const { devId } = req.params;
+        const { user } = req.headers;
+
+        const loggedDev = await Dev.findById(devId);
+        const targetDev = await Dev.findById(user);
+
+        if (!targetDev) {
+            return res.status(400).json({ error: "Dev not exists" })
+        }
+
+        if (targetDev.likes.includes(loggedDev._id)) {
+            console.log("Deus match")
+        }
+
+        loggedDev.likes.push(targetDev._id);
+
+        await loggedDev.save();
+
+        return res.json(loggedDev);
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,12 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require('cors');
 
 const routes = require('./routes');
 
 const server = express();
 
 mongoose.connect('mongodb+srv://oministack:oministack@cluster0-fb2v2.mongodb.net/oministack8?retryWrites=true&w=majority', { useNewUrlParser: true });
-
+server.use(cors);
 server.use(express.json());
 server.use(routes);
 
